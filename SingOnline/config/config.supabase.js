119f27a70b2419b2a02fcf01e480ba0d3048bf7a
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-05-16 09:14:23 +0200

    New changes to data

--- SingOnline/config/config.supabase.js
+++ SingOnline/config/config.supabase.js
@@ -1,35 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import dontenv from 'dotenv'
+import dotenv from 'dotenv'
 dotenv.config()
 
-
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://mqazwgooiooaafjhlaje.supabase.co'
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
-import { supabase } from './Config/supabase.config.js';
-
-app.get('/test', (req, res) => {
-    res.send('Dette er en test route!');
-});
-
-let { data, error } = await supabase.from('songs').select('id, title')
-if (error) {
-    throw new Error(error.message);
-} else {
-    return data
-}
-
-// app.get('/test', async (req, res) => {
-//     try {
-//         let { data, error } = await supabase.from('songs').select('id, title');
-//         if (error) {
-//             throw new Error(error.message);
-//         } else {
-//             res.json(data);
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// });
--- SingOnline/index.js
+++ SingOnline/index.js
@@ -1,9 +1,21 @@
 import express from 'express' 
+import { supabase } from './config/config.supabase.js'
 const port = 4000
 const app = express()
 
-app.get('/',(req, res)=>{
+app.get('/', async (req, res)=>{
+    const {data, error} = await supabase
+    .from('Songs')
+    .select('title')
+    if(error) {
+        console.error(error)
+    } else {
+        console.log(data)
+    }
+   
+    
     res.send('Welcome')
+
 })
 
 app.listen(port, ()=> {
